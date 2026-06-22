aa3d044a6c94e0d13cb6d9907565b06b5b53228f
Ajay Gupta <relaxgameing8@gmail.com>
2026-06-22 22:08:42 +0530

    feat: implemented A* pathfinding needs to be tested

--- Assets/Script/Grid/GridSystem.cs
+++ Assets/Script/Grid/GridSystem.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using Quaternion = UnityEngine.Quaternion;
 using Vector2 = UnityEngine.Vector2;
@@ -12,7 +13,12 @@ public class GridSystem : MonoBehaviour {
     private bool _isEditing;
 
     private GameObject _selectedObstacle;
-    [SerializeField ]private List<GameObject> _tiles;
+    // future optimization:
+    // could have each tile ref of all the surrounding 8 tiles like a linklist
+    // to avoid accessing this massive list;
+    // or
+    // add chunk based system
+    [SerializeField] private List<GameObject> _tiles;
 
     public Vector2 GridDimension => new Vector2(gridRows, gridCols);
 
@@ -25,6 +31,26 @@ public class GridSystem : MonoBehaviour {
             Mathf.FloorToInt(worldPos.z) / gridSize);
     }
 
+    #region Utils
+
+    public bool CellWithInGrid(Vector2 cellNo) {
+        if (cellNo.y < 0 || cellNo.y >= gridCols) {
+            return false;
+        }
+
+        if (cellNo.x < 0 || cellNo.x >= gridRows) {
+            return false;
+        }
+
+        return true;
+    }
+
+
+    // returns tile gameobject from a given cell number
+    public GameObject GetTileFromCellNumber(Vector2 cellNo) {
+        return _tiles[(int)(cellNo.x * gridRows + cellNo.y)];
+    }
+    #endregion
 
 
     #region GridGeneration
@@ -51,12 +77,163 @@ public class GridSystem : MonoBehaviour {
     public void Clear() {
         var tiles = GameObject.FindGameObjectsWithTag("Tile");
         foreach (GameObject tile in tiles) {
-            #if UNITY_EDITOR
-                DestroyImmediate(tile);
-            #else
+#if UNITY_EDITOR
+            DestroyImmediate(tile);
+#else
                 Destroy(tile);
-            #endif
+#endif
         }
+    }
+
+    #endregion
+
+    #region PathFinding
+
+    class Node  {
+        public Vector2Int pos;
+        public int gCost; // cost for start -> cur
+        public int hCost; // cost for cur -> end
+        public Node cameFrom;
+
+        public Node(Vector2Int pos, int gCost, int hCost, Node cameFrom) {
+            this.pos = pos;
+            this.gCost = gCost;
+            this.hCost = hCost;
+            this.cameFrom = cameFrom;
+        }
+    }
+
+    class NodeComparer : IComparer<Node>
+    {
+        public int Compare(Node x, Node y) {
+            if (x == null) return 1;
+            if (y == null) return -1;
+
+            int xCost = x.gCost + x.hCost;
+            int yCost = y.gCost + y.hCost;
+
+            if (xCost == yCost) {
+                if (x.hCost < y.hCost) {
+                    return -1;
+                }
+
+                if (x.hCost == y.hCost) {
+                    if (x.gCost < y.gCost) {
+                        return -1;
+                    }
+
+                // this whole 'if' is handling the case where both node has same
+                // h and g cost. making us rely on cell coordinate as cell coordinate
+                // will always be different for different cell
+                    if (x.gCost == y.gCost) {
+                        if (x.pos.x.CompareTo(y.pos.x) != 0) {
+                            return x.pos.x.CompareTo(y.pos.x);
+                        }
+
+                        return x.pos.y.CompareTo(y.pos.y);
+                    }
+                }
+
+                return 1;
+            }
+
+            return xCost < yCost ? -1 : 1;
+        }
+    }
+
+    int CityBlockDist(Vector2 a, Vector2 b) {
+        return (int)Mathf.Abs(a.x - b.x) + (int)Mathf.Abs(a.y - b.y);
+    }
+
+    // find the shortest path between cell coord A to B using A* Algo
+    // using city block distance
+    public  List<Vector2> PathFromAToB(Vector2Int start, Vector2Int end) {
+        if (!CellWithInGrid(start) || !CellWithInGrid(end)) {
+            return null;
+        }
+
+        var nodeComparer = new NodeComparer();
+        SortedSet<Node> notVisited = new SortedSet<Node>(nodeComparer);
+        Dictionary<Vector2Int, int> visited = new Dictionary<Vector2Int, int>();
+
+        notVisited.Add(new Node(start, 0, CityBlockDist(start, end), null));
+
+        Node Destination = null;
+        while (notVisited.Count > 0) {
+            var cur = notVisited.Min;
+            notVisited.Remove(cur);
+
+            if (visited.TryGetValue(cur.pos , out int bestCost) && bestCost <= cur.gCost){
+                continue;
+            }
+
+
+            if (cur.pos == end) {
+                Destination = cur;
+                break;
+            }
+
+            if (!CellWithInGrid(cur.pos)) {
+                continue;
+            }
+
+
+            // this part could be optimized for a chunk by having a set to store
+            // all the cells which are occupied
+            GameObject tile = GetTileFromCellNumber(cur.pos);
+            if(tile.GetComponent<TileController>().IsOccupied) continue;
+
+            visited[cur.pos] = cur.gCost;
+            // add all 4 neighbours
+            var nodePos = new Vector2Int(cur.pos.x , cur.pos.y);
+
+            nodePos.x++; // x+1
+            notVisited.Add(new Node(
+                nodePos,
+                cur.gCost + 1,
+                CityBlockDist(nodePos, end),
+                cur
+            ));
+
+            nodePos.x -= 2; // x-1
+            notVisited.Add(new Node(
+                nodePos,
+                cur.gCost + 1,
+                CityBlockDist(nodePos, end),
+                cur
+            ));
+            nodePos.x++; // x
+
+            nodePos.y++; // y + 1
+            notVisited.Add(new Node(
+                nodePos,
+                cur.gCost + 1,
+                CityBlockDist(nodePos, end),
+                cur
+            ));
+            nodePos.y -= 2; // y -1
+            notVisited.Add(new Node(
+                nodePos,
+                cur.gCost + 1,
+                CityBlockDist(nodePos, end),
+                cur
+            ));
+
+        }
+
+        if (Destination == null) {
+            return null;
+        }
+
+        List<Vector2> path = new List<Vector2>();
+        Node curNode = Destination;
+        while (curNode.cameFrom != null) {
+            path.Add(curNode.pos);
+            curNode = curNode.cameFrom;
+        }
+        path.Add(curNode.pos);
+        path.Reverse();
+        return  path;
     }
 
     #endregion
